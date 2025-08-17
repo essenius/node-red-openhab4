5cebf31808464a71d6dc04b2c46e30b49f1a53b7
essenius <essenius@users.noreply.github.com>
2025-08-17 15:11:11 +0200

    Cleaned up test generation

--- examples/generate-test-flow.js
+++ examples/generate-test-flow.js
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 // Copyright 2025 Rik Essenius
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
@@ -9,7 +11,6 @@
 // distributed on an "AS IS" BASIS WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and limitations under the License.
 
-#!/usr/bin/env node
 // Script to generate test-flow.json with environment-specific configuration
 
 const fs = require('fs');
